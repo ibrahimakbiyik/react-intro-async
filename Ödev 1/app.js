c276a0c3775a86dd645b70149b13ecde8c9499f8
Karaca Gürbüz <99208404+KaracaGurbuz@users.noreply.github.com>
2022-05-31 21:37:14 +0300

    changed export to export default.

--- Ödev 1/app.js
+++ Ödev 1/app.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getData = async (user_id) => {
+export default const getData = async (user_id) => {
     const {data:User} = await axios("https://jsonplaceholder.typicode.com/users/"+user_id)
     const {data:Posts} = await axios("https://jsonplaceholder.typicode.com/posts?userId="+user_id)
     
